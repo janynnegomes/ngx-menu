d5800f9b6a71e67514282815ee261f2fd209df4f
janynnegomes <janynnegomes@gmail.com>
2020-11-03 20:54:42 -0300

    import font awesome to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, SharedModule],
+  imports: [BrowserModule, FontAwesomeModule, FormsModule, SharedModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
