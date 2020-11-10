48c6818991e2c0f1e4738ecac36b8706b7935e02
janynnegomes <janynnegomes@gmail.com>
2020-11-10 00:20:56 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { SharedModule } from "./shared/shared.module";
 import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, CommonModule, FormsModule, SharedModule],
+  imports: [BrowserModule,  CommonModule, FormsModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
